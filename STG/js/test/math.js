0beef36f4110faf69c87c48b26a5077a292b8408
liukuo <liukuo@gitlab.com>
2018-08-31 13:59:14 +0800

    commit

--- STG/js/test/math.js
+++ STG/js/test/math.js
@@ -1,5 +1,80 @@
-console.log('math')
-var x=1;
-var add = function(a, b){
-	return a + b;
-}
+// var add = (a, b) => a + b;
+// var x = 100, y = 200;
+
+// exports.add = add;
+// exports.x = x;
+// // module.exports.add = add;
+
+// console.log(module);
+// // console.log(exports);
+// // console.log(module.exports);
+// // console.log(module.exports === exports);
+
+
+// var x = 5;
+// var addX = function (value) {
+// 	return value + x;
+// };
+// module.exports.x = x;
+// module.exports.addX = addX;
+
+
+// var firstName = 'Michael';
+// var lastName = 'Jackson';
+// var year = 1958;
+
+// export {firstName, lastName, year};
+
+// export var m = 1;
+
+// var m = 1;
+// export {m};
+
+// var n = 1;
+// export {n as m};
+
+// export default function() {
+// 	console.log('foo');
+// }
+
+// var a = {name: 1};
+// var b = a;
+
+// console.log(a);
+// console.log(b);
+
+// b.name = 2;
+// console.log(a);
+// console.log(b);
+
+// b = {name: 3};
+// console.log(a);
+// console.log(b);
+
+// b = a;
+// console.log(a);
+// console.log(b);
+
+// a.name = 4;
+// console.log(a);
+// console.log(b);
+
+// a = {name: 5};
+// console.log(a);
+// console.log(b);
+
+
+// const D = 123;
+// // export default D;
+// export {D as default};
+
+
+// export function cube(x){
+// 	return x * x * x;
+// }
+// const foo = Math.PI + Math.SQRT2;
+// export {foo};
+
+// export default function(x){
+// 	return x * x;
+// }
--- STG/js/test/module.js
+++ STG/js/test/module.js
@@ -1,3 +1,28 @@
-var math = require('math');
+// var math = require('math');
+// var math = require('./math.js');
 
-console.log(math.x);
+
+// console.log(math);
+// console.log(math.add(2, 3));
+// console.log(math.x);
+
+
+// console.log(math.x);
+// console.log(math.addX(1));
+
+
+// console.log(math.firstName, math.lastName, math.year);
+// console.log(math.m);
+// math.default();
+
+
+// import { cube, foo } from 'math';
+// console.log(cube(3));
+// console.log(foo);
+
+// import {cube} from 'math';
+// console.log(cube(3));
+
+// import square from 'math';
+// console.log(square, typeof square);
+// console.log(square(3));
