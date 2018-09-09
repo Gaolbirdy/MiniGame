3bf1514e01e1faf5bb5cde5a5ac1f6369fab0a73
Gaolbirdy <gaolbird@yeah.net>
2018-09-09 16:08:56 +0800

    commit

--- STG/js/libs/symbol.js
+++ STG/js/libs/symbol.js
@@ -6,6 +6,12 @@
 let Symbol = window.Symbol;
 let idCounter = 0;
 
-if(Symbol) {
-    consolelog(1)
+if(!Symbol) {
+    Symbol = function Symbol(key) {
+        return `__${key}_${Math.floor(Math.random() * 1e9)}_${++idCounter}__`;
+    };
+
+    Symbol.iterator = Symbol('Symbol.iterator');
 }
+
+window.Symbol = Symbol;
