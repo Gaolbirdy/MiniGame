0c122ed7c4fdb452a890c37d38bb470adaee633f
liukuo <liukuo@gitlab.com>
2018-09-14 18:58:03 +0800

    commit

--- Timer/game.js
+++ Timer/game.js
@@ -1,6 +1,19 @@
 const canvas = wx.createCanvas();
 const context = canvas.getContext('2d');
 
+// UI绘制
 context.fillStyle = 'yellow';
 context.font = '30px Arial';
-context.fillText('hello', 50, 50);
+context.textAlign = 'center';
+context.fillText('时间感觉者', canvas.width / 2, canvas.height / 8);
+context.fillText('秒时触摸屏幕', canvas.width / 2, canvas.height / 3);
+
+
+
+// 随机时间目标
+
+
+// 触摸事件监听
+
+
+// 判断触摸时间成绩
