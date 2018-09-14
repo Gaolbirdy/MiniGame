616abd90c8161e10d51fdc75fc23b3462421333e
gaolbird <gaolbird@yeah.net>
2018-09-14 22:33:28 +0800

    commit

--- Timer/game.js
+++ Timer/game.js
@@ -2,21 +2,36 @@ const canvas = wx.createCanvas();
 const context = canvas.getContext('2d');
 
 let timeGoal = 1;
-let deviation = 0.25;
+let bestDeviation = 0.25;
+let goodDeviation = bestDeviation * 2;
+let normalDeviation = bestDeviation * 4;
 let time = 0;
-let result = '感觉如何？';
 let lastTime = 0;
+let combo = 0;
+let comboRecord = combo;
+let slowV = 0;
+let fastV = 0;
 
 const MINTIME = 1;
 const MAXTIME = 7;
 
-const GAMETITLE = '时间感觉者';
-const TIP = ' 秒后  及时触摸屏幕';
+let result = '感觉如何？';
+const GAMETITLE = '时间猎手';
+const TIP1 = ' 秒后  及时触摸屏幕';
+const TIP2 = '抓住时间';
 const STYLE = 'yellow';
-const FONT = '30px Arial';
+const FONT = '25px Arial';
 const ALIGN = 'center';
-const winText = '准';
-const lostText = '感觉不对';
+const goodText = '准';
+const badText = '感觉离谱';
+const bestText = '神准';
+const normalText = '感觉微妙';
+const slowText = "'慢性子'   ";
+const fastText = "'急性子'   ";
+const comboText = '连准: ';
+const slowVText = '    慢性值:  ';
+const fastVText = '    急性值:  ';
+const recordText = '最大连准: ';
 
 start();
 
@@ -49,19 +64,37 @@ wx.onTouchStart((res) => {
 
 // 得到触摸的用时
 function getTime(res) {
-    time = res.timeStamp - lastTime;
+    time = res.timestamp - lastTime;
 
-    lastTime = res.timeStamp;
+    lastTime = res.timestamp;
 }
 
 // 根据触摸时间，判定成绩结果
 function getResult(time) {
     time = time / 1000;
 
-    if (time <= timeGoal + deviation && time >= timeGoal - deviation) {
-        result = winText;
+    if (time <= timeGoal + bestDeviation && time >= timeGoal - bestDeviation) {
+        result = bestText;
+        combo++;
+    } else if (time <= timeGoal + goodDeviation && time >= timeGoal - goodDeviation) {
+        result = goodText;
+        combo++;
+    } else if (time <= timeGoal + normalDeviation && time >= timeGoal - normalDeviation) {
+        result = normalText;
+        combo = 0;
     } else {
-        result = lostText;
+        result = badText;
+        combo = 0;
+    }
+
+    combo > 1 && combo > comboRecord ? comboRecord = combo : null;
+
+    if (time > timeGoal) {
+        result = slowText + result;
+        slowV++;
+    } else {
+        result = fastText + result;  
+        fastV++
     }
 }
 
@@ -72,6 +105,11 @@ function render() {
     context.font = FONT;
     context.textAlign = ALIGN;
     context.fillText(GAMETITLE, canvas.width / 2, canvas.height / 8);
-    context.fillText(timeGoal + TIP, canvas.width / 2, canvas.height / 3);
-    context.fillText(result, canvas.width / 2, canvas.height / 3 * 2);
+    context.fillText(timeGoal + TIP1, canvas.width / 2, canvas.height / 4);
+    context.fillText(TIP2, canvas.width / 2, canvas.height / 4 * 1.3);
+    context.fillText(result, canvas.width / 2, canvas.height / 10 * 5);
+    combo > 1 ? context.fillText(comboText + combo, canvas.width / 2, canvas.height / 10 * 6) : null;
+    context.fillText(fastVText + fastV + slowVText + slowV, canvas.width / 2, canvas.height / 10 * 8);
+    context.fillText(recordText + comboRecord, canvas.width / 2, canvas.height / 10 * 9);
+    
 }
